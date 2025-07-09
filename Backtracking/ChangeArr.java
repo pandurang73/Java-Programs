3961dd0955ddd726e59dce3fd1ae985b108b64b1
Pandurang73 <pandurangmhaske111@gmail.com>
2025-07-09 21:55:27 +0530

    new code

---
+++ Backtracking/ChangeArr.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class ChangeArr {
+    public static void changeArr(int arr[], int i, int val) {
+        if (arr.length == i) {
+            // System.out.println(Arrays.toString(arr));
+            return;
+        }
+
+        arr[i] = val;
+        changeArr(arr, i + 1, val + 1);
+        arr[i] = arr[i] - 2;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = new int[5];
+        changeArr(arr, 0, 1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
